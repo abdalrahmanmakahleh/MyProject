304359b1c5f9c0d3ec596ba67c450afcd0961048
92luay <92luay@gmail.com>
2018-09-30 00:52:42 +0300

    updates

--- src/app/views/appviews/appviews.module.ts
+++ src/app/views/appviews/appviews.module.ts
@@ -2,31 +2,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-
 import { StarterViewComponent } from './starterview.component';
 import { LoginComponent } from './login.component';
-
-
 import { SparklineModule } from '../../components/charts/sparkline';
 import { PeityModule } from '../../components/charts/peity';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FileUploadModule } from 'ng2-file-upload';
 import { HttpClientModule } from '@angular/common/http';
+import { DataTablesModule } from 'angular-datatables';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [
     StarterViewComponent,
     LoginComponent
   ],
   imports: [
-    BrowserModule,
     RouterModule,
     PeityModule,
     SparklineModule,
     FormsModule,
     ReactiveFormsModule,
     FileUploadModule,
-    HttpClientModule
-
+    HttpClientModule,
+    BrowserModule,
+    DataTablesModule,
+    NgbModule
   ],
   exports: [
     StarterViewComponent,
--- src/app/views/appviews/starterview.component.ts
+++ src/app/views/appviews/starterview.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FileUploader } from 'ng2-file-upload';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Country } from '../../models/country';
 import { environment } from '../../../environments/environment';
+import { Subject } from 'rxjs';
+// import 'rxjs/add/operator/map';
+
 
 @Component({
   selector: 'app-starter',
@@ -15,9 +18,25 @@ export class StarterViewComponent implements OnInit {
   countries: Country[];
   urlLoad: string;
   filePath: string;
-  constructor(private http: HttpClient) {}
+  dtOptions: DataTables.Settings = {};
+  dtTrigger: Subject<object> = new Subject();
+  Name: string;
+  Name2: string;
+  Nationality: string;
+  ST_CUR_COD: string;
+  Refernce_No: string;
+  Loc_Status: number;
+  Phone_Code: number;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
+
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10
+    };
+
     this.country = new Country();
 
     this.url = environment.url + 'core';
@@ -33,6 +52,15 @@ export class StarterViewComponent implements OnInit {
     this.countries = [];
     this.loadCountries();
   }
+
+  loadCountries() {
+    this.http.get<Country[]>(this.url + '/LoadCountries').subscribe(res => {
+      this.countries = res;
+      this.dtTrigger.next();
+
+    });
+  }
+
 
   UploadFlag() {
     this.uploader.uploadAll();
@@ -65,11 +93,7 @@ export class StarterViewComponent implements OnInit {
     });
   }
 
-  loadCountries() {
-    this.http.get<Country[]>(this.url + '/LoadCountries').subscribe(res => {
-      this.countries = res;
-    });
-  }
+
 
   deleteCountry(id) {
     this.http
@@ -80,10 +104,18 @@ export class StarterViewComponent implements OnInit {
       });
   }
 
-  updateCountry(country: Country) {
-    this.http.post(this.url + '/UpdateCountry', country).subscribe(res => {
-      alert(res);
-      this.loadCountries();
-    });
+  updateCountry(form, country: Country) {
+    this.Name = country.Name;
+    this.Name2 = country.Name2;
+    this.Nationality = country.Nationality;
+    this.ST_CUR_COD = country.ST_CUR_COD;
+    this.Refernce_No = country.Refernce_No;
+    this.Loc_Status = country.Loc_Status;
+    this.Phone_Code = country.Phone_Code;
+
+    // this.http.post(this.url + '/UpdateCountry', country).subscribe(res => {
+    //   alert(res);
+    //   this.loadCountries();
+    // });
   }
 }
